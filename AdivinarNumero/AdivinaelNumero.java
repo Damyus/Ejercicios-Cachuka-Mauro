e7d1e969b9a3c35ffb802755e364546c6c5f191b
Mauro <mcachuka@gmail.com>
2022-10-02 19:54:00 -0300

    Quinto commit

--- AdivinarNumero/AdivinaelNumero.java
+++ AdivinarNumero/AdivinaelNumero.java
@@ -7,15 +7,31 @@ public class AdivinaelNumero {
     public void jugar() {
         System.out.println("Hola, cuál es tu nombre?");
         String nombrejugador = Entrada.nextLine();
-        System.out.printf("Bienvenido %s, vamos a comenzar\n");
+        System.out.printf("Bienvenido %s, vamos a comenzar\n", nombrejugador);
 
-        while(juegoactivo);
-        int min = 0;
-        int max = 100;
-        int numerojuego = (int) Math.random() * ((max - min) + 1);
+        while(juegoactivo) {
+            int intentos = 0;
+            int min = 0;
+            int max = 100;
+            int numerojuego = (int) (Math.random() * ((max - min) + 1));
 
-        System.out.println(juegoactivo);
-        juegoactivo = false;
+            System.out.printf("%s, he esogido un numero entre %d y %d, adivinalo", nombrejugador, min, max);
+            int numerojugador;
+
+            System.out.println("escoge un numero");
+            numerojugador = Entrada.nextInt();
+
+            if (numerojuego < numerojugador) {
+                System.out.println("muy alto, adivina otra vez");
+            } else if(numerojuego > numerojugador) {
+                System.out.println("muy bajo, adivina otra vez");
+            }
+            intentos++;
+
+
+
+            juegoactivo = false;
+        }
 
     }
 }
