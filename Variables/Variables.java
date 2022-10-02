789d70dda1bae725be42f8ee634d679c58b6092b
Mauro <mcachuka@gmail.com>
2022-10-02 16:35:41 -0300

    segundo commit

---
+++ Variables/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main (String[] args) {
+        int hamburguesas = 6;
+        int personas = 2;
+        int hamburguesasPorpersonas = hamburguesas / personas;
+
+        System.out.printf("Si hay %d hamburguesas y %d personas, entonces le tocan %d de hamburguesas a cada persona", hamburguesas, personas, hamburguesasPorpersonas);
+
+
+    }
+}
