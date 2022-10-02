60c7755ba9e13eb9d032bc0bf2a1ef510bfb0287
Mauro <mcachuka@gmail.com>
2022-10-02 16:56:16 -0300

    Segundo commit

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -4,7 +4,19 @@ public class Variables {
         int personas = 2;
         int hamburguesasPorpersonas = hamburguesas / personas;
 
-        System.out.printf("Si hay %d hamburguesas y %d personas, entonces le tocan %d de hamburguesas a cada persona", hamburguesas, personas, hamburguesasPorpersonas);
+        System.out.printf("Si hay %d hamburguesas y %d personas, entonces le tocan %d de hamburguesas a cada persona\n", hamburguesas, personas, hamburguesasPorpersonas);
+
+        String auto = "Lamborghini";
+        Double velocidad = 90.3;
+
+        System.out.printf("el %s se desplaza a %f km/h\n", auto, velocidad);
+
+        int precioleche = 14;
+        double preciojamon = 20.5;
+        int precioqueso= 15;
+
+        double preciototal = precioleche + preciojamon + precioqueso;
+        System.out.printf("el precio total de las compras es de %s", preciototal);
 
 
     }
